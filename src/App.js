b6d4ab1365e6fe366699e73dc0feb2b1c58aebe9
Пользователь <vin123@mail.ru>
2021-06-02 16:14:20 +0300

    add page ChoiseProgram

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import  ReactDOM  from 'react-dom';
 import MainPage from './pages/MainPages'
 import RegistrationPage from "./pages/RegistrationPage";
 import {  Route  } from 'react-router-dom';
-
+import ChoiseProgram from "./pages/ChoiseProgram"
 
 import '../src/index.css';
 
@@ -13,10 +13,10 @@ class App extends Component {
         <div className="app">
           <Route path="/" exact component={MainPage} />
           <Route path="/registeration" exact component={RegistrationPage} />
+          <Route path="/choise" exact component={ChoiseProgram} />
           {/* <Route path="/schema/:id" exact component={SchemaPage} />
           <Route path="/schema/:id" exact component={SchemaPage} />
-          <Route path="/schema/:id" exact component={SchemaPage} />
-          <Route path="/schema/:id" exact component={SchemaPage} /> */}
+          <Route path="/schema/:id" exact component={SchemaPage} />  */}
         </div>
       );
     }
