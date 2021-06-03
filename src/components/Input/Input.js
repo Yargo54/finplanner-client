630ebcf26c60bbf199276b2d0e36de635b959773
yudin yaroslav <yarikyudin@mail.ru>
2021-06-03 11:38:11 +0300

    accumulation page

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Input.css"
 
-export default function Input ( { text, content, classInputPContent } ) {
+export default function Input ( { text, content, classInputPContent, classInput } ) {
     if(content && classInputPContent === "p-register"){
         return (
             <div className="div-input-style">
@@ -10,7 +10,7 @@ export default function Input ( { text, content, classInputPContent } ) {
                 <p className="p-register">{content}</p>
             </div>
         )
-    } else if (!content) {
+    } else if (!content && !classInput) {
         return (
             <div className="div-input-style">
                 <p className="p-input-style">{text}</p>
@@ -23,6 +23,21 @@ export default function Input ( { text, content, classInputPContent } ) {
                 <p className="p-input-style">{text}</p>
                 <input type="text" className="input-style" />
                 <a href=""><p className="p-log">{content}</p></a>
+            </div>
+        )
+    } else if (classInput === "accumulation" && content && classInputPContent === "accumulation-p") {
+        return (
+            <div className="div-input-accumulation-style">
+                <p className="accumulation-p">{text}</p>
+                <input type="text" className="accumulation" />
+                <p className="p-accumulation-content">{content}</p>
+            </div>
+        )
+    } else if (classInput === "accumulation" && !content && classInputPContent === "accumulation-p") {
+        return (
+            <div className="div-input-accumulation-style">
+                <p className="accumulation-p">{text}</p>
+                <input type="text" className="accumulation" />
             </div>
         )
     }
