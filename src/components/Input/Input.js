16ca27f3ce0c226145bc203d9332b1e4b62c6ef4
Пользователь <vin123@mail.ru>
2021-06-03 18:40:10 +0300

    add handle AccumulationProgramPage,LogPage,RegistrationPage,MainPages

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Input.css"
 
-export default function Input ( { name, text, content, classInputPContent } ) {
+export default function Input ( { name, text, content, classInputPContent, classInput }  ) {
+
     if(content && classInputPContent === "p-register"){
         return (
             <div className="div-input-style">
                 <p className="p-input-style">{text}</p>
-                <input name={name} type="text" className="input-style" />
+                <input name={name}  type="text" className="input-style" />
                 <p className="p-register">{content}</p>
             </div>
         )
@@ -14,22 +15,23 @@ export default function Input ( { name, text, content, classInputPContent } ) {
         return (
             <div className="div-input-style">
                 <p className="p-input-style">{text}</p>
-                <input name={name} type="text" className="input-style" />
+                <input name={name}  type="text" className="input-style" />
+                {/* <input name={name} value type="text" className="input-style" /> */}
             </div>
         )
     } else if (content && classInputPContent === "p-log") {
         return (
             <div className="div-input-style">
                 <p className="p-input-style">{text}</p>
-                <input name={name} type="text" className="input-style" />
-                <a href=""><p className="p-log">{content}</p></a>
+                <input name={name} type="text"  className="input-style" />
+                <a href="/forgotpassword"><p className="p-log">{content}</p></a>
             </div>
         )
     } else if (classInput === "accumulation" && content && classInputPContent === "accumulation-p") {
         return (
             <div className="div-input-accumulation-style">
                 <p className="accumulation-p">{text}</p>
-                <input type="text" className="accumulation" />
+                <input type="text"  className="accumulation" />
                 <p className="p-accumulation-content">{content}</p>
             </div>
         )
@@ -37,7 +39,7 @@ export default function Input ( { name, text, content, classInputPContent } ) {
         return (
             <div className="div-input-accumulation-style">
                 <p className="accumulation-p">{text}</p>
-                <input type="text" className="accumulation" />
+                <input type="text"  className="accumulation" />
             </div>
         )
     }
--- src/pages/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage.js
@@ -5,12 +5,20 @@ import Button from "../components/Button/Button.js";
 import "./ForgotPasswordPage.css";
 
 export default function ForgotPasswordPade () {
+
+    let handleSubmit = (event) => {
+        alert("Вспомни пожалуйста пароль. Мы ничего не записали :):):):')")
+    }
+
+
     return (
         <div className="div-forgot">
             <div className="div-forgot-content">
                 <h2 className="h2-forgot">Введите email адрес</h2>
+                <form onSubmit={(event) => { handleSubmit(event) }}>
                 <Input text="email" content="Введите адрес электронной почты, на которую необходимо отправить код для сброса пароля" classInputPContent="p-register"/>
-                <Button text="Отправить код" classbutton="button-forgot" /> 
+                <Button text="Отправить код" classbutton="button-forgot" />
+                 </form>
             </div>
         </div>
     )
