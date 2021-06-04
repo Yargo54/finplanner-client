c6a5350069ad72032365594c474f2138e7195b8c
yudin yaroslav <yarikyudin@mail.ru>
2021-06-04 15:08:07 +0300

    accumulationprogrammpage first

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,44 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Input.css"
 
-export default function Input ( { text, content, classInputPContent, classInput } ) {
-    if(content && classInputPContent === "p-register"){
-        return (
-            <div className="div-input-style">
-                <p className="p-input-style">{text}</p>
-                <input type="text" className="input-style" />
-                <p className="p-register">{content}</p>
-            </div>
-        )
-    } else if (!content && !classInput) {
-        return (
-            <div className="div-input-style">
-                <p className="p-input-style">{text}</p>
-                <input type="text" className="input-style" />
-            </div>
-        )
-    } else if (content && classInputPContent === "p-log") {
-        return (
-            <div className="div-input-style">
-                <p className="p-input-style">{text}</p>
-                <input type="text" className="input-style" />
-                <a href=""><p className="p-log">{content}</p></a>
-            </div>
-        )
-    } else if (classInput === "accumulation" && content && classInputPContent === "accumulation-p") {
-        return (
-            <div className="div-input-accumulation-style">
-                <p className="accumulation-p">{text}</p>
-                <input type="text" className="accumulation" />
-                <p className="p-accumulation-content">{content}</p>
-            </div>
-        )
-    } else if (classInput === "accumulation" && !content && classInputPContent === "accumulation-p") {
-        return (
-            <div className="div-input-accumulation-style">
-                <p className="accumulation-p">{text}</p>
-                <input type="text" className="accumulation" />
-            </div>
-        )
-    }
+export default function Input ( { text, content, classInputPContent, classInput, countConvert, countOsnova, countCel, osnova} ) {
+
+    return(
+        <div className={classInput === "accumulation" ? "div-input-accumulation-style" : "div-input-style"}>
+            <p className={classInput === "accumulation" ? "accumulation-p" : "p-input-style"}>
+                {text}
+            </p>
+            <input 
+                type="text" 
+                className={classInput === "accumulation" ? "accumulation" : "input-style"}
+            />
+            {content ? 
+            <p 
+                className={classInputPContent === "p-register" ? "p-register" : "p-log" || classInputPContent === "accumulation-p" ? "accumulation-p" : null}>
+                {content}
+            </p> : null}
+        </div>
+    )
+    
+    // if(content && classInputPContent === "p-register"){
+    //     return (
+    //         <div className="div-input-style">
+    //             <p className="p-input-style">{text}</p>
+    //             <input type="text" className="input-style"/>
+    //             <p className="p-register">{content}</p>
+    //         </div>
+    //     )
+    // } else if (!content && !classInput) {
+    //     return (
+    //         <div className="div-input-style">
+    //             <p className="p-input-style">{text}</p>
+    //             <input type="text" className="input-style" />
+    //         </div>
+    //     )
+    // } else if (content && classInputPContent === "p-log") {
+    //     return (
+    //         <div className="div-input-style">
+    //             <p className="p-input-style">{text}</p>
+    //             <input type="text" className="input-style" />
+    //             <a href=""><p className="p-log">{content}</p></a>
+    //         </div>
+    //     )
+    // } else if (classInput === "accumulation" && content && classInputPContent === "accumulation-p" && osnova) {
+    //     return (
+    //         <div className="div-input-accumulation-style">
+    //             <p className="accumulation-p">{text}</p>
+    //             <input type="text" className="accumulation" defaultValue={countOsnova}/>
+    //             <p className="p-accumulation-content">{content}</p>
+    //         </div>
+    //     )
+    // } else if (classInput === "accumulation" && content && classInputPContent === "accumulation-p") {
+    //     return (
+    //         <div className="div-input-accumulation-style">
+    //             <p className="accumulation-p">{text}</p>
+    //             <input type="text" className="accumulation" defaultValue={countCel}/>
+    //             <p className="p-accumulation-content">{content}</p>
+    //         </div>
+    //     )
+    // } else if (classInput === "accumulation" && !content && classInputPContent === "accumulation-p") {
+    //     return (
+    //         <div className="div-input-accumulation-style">
+    //             <p className="accumulation-p">{text}</p>
+    //             <input type="text" className="accumulation" defaultValue={countConvert}/>
+    //         </div>
+    //     )
+    // }
 }
--- src/pages/ChoiseProgram.js
+++ src/pages/ChoiseProgram.js
@@ -1,39 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import './ChoiseProgram.css';
 
 
 function ChoiseProgram() {
+    
+    let dataAccumulation;
+
+    let [ convert, setConvert ] = useState('');
+    let [ convertDescription, setConvertDescription ] = useState('');
+
+    let [ jug, setJug] = useState('');
+    let [ jugDescription, setJugDescription ] = useState('');
+
+    let [ safe, setSafe] = useState('');
+    let [ safeDescription, setSafeDescription ] = useState('');
+
+    let [ zeroing, setZeroing] = useState('');
+    let [ zeroingDescription, setZeroingDescription ] = useState('');
+
+    let [ latte, setLatte] = useState('');
+    let [ latteDescription, setLatteDescription ] = useState('');
+
+    let [ popular, setPopular] = useState('');
+    let [ popularDescription, setPopularDescription ] = useState('');
+
+    useEffect(() => {
+        fetch("http://localhost:3000/accumulation")
+        .then(res => res.json())
+        .then((data) => {
+            dataAccumulation = [...data];
+            dataAccumulation.forEach(element => {
+                switch(element.name){
+                    case "Четыре конверта":
+                        setConvert(element.name);
+                        setConvertDescription(element.short);
+                        break;
+                    case "Шесть кувшинов":
+                        setJug(element.name);
+                        setJugDescription(element.short);
+                        break;
+                    case "Сейф":
+                        setSafe(element.name);
+                        setSafeDescription(element.short);
+                        break;
+                    case "Обнуление":
+                        setZeroing(element.name);
+                        setZeroingDescription(element.short);
+                        break;
+                    case "Эффект латте":
+                        setLatte(element.name);
+                        setLatteDescription(element.short);
+                        break;
+                    case "50/30/20":
+                        setPopular(element.name);
+                        setPopularDescription(element.short);
+                        break;
+                }
+            });
+        })
+    }, [])
+
+    let history = useHistory();
+
+    function onClickConvert() {
+        history.push('/accumulationprogramconvert');
+    }
+
+    function onClickJug() {
+        history.push('/accumulationprogramjug');
+    }
+
+    function onClickSafe() {
+        history.push('/accumulationprogramsafe');
+    }
+
+    function onClickZeroing() {
+        history.push('/accumulationprogramzeroing');
+    }
+
+    function onClickLatte() {
+        history.push('/accumulationprogramlatte');
+    }
+
+    function onClickPopular() {
+        history.push('/accumulationprogrampopular');
+    }
+    
     return (
         <div>
             <div className="main">
                 <div className='container'>
                     <p className="schema">Выберете схему накопления</p>
 
-                    <button className="envelope-img"></button>
-                    <p className="four-envelope" >Метод “Четыре конверта”</p>
-                    <p className="four-envelope_description">description</p>
+                    <button className="envelope-img" onClick={onClickConvert}></button>
+                    <p className="four-envelope" >Метод "{convert}"</p>
+                    <p className="four-envelope_description">{convertDescription}</p>
 
-                    <button className="jag-img"></button>
-                    <p className="six-jag">Метод “Шесть кувшинов”</p>
-                    <p className="six-jag_description">description</p>
+                    <button className="jag-img" onClick={onClickJug}></button>
+                    <p className="six-jag">Метод "{jug}"</p>
+                    <p className="six-jag_description">{jugDescription}</p>
 
-                    <button className="vault-img"></button>
-                    <p className="vault">Метод “Сейф”</p>
-                    <p className="vault_description">description</p>
-
-
-                    <button className="null-img"></button>
-                    <p className="null">Метод “Обнуление”</p>
-                    <p className="null_description">description</p>
+                    <button className="vault-img" onClick={onClickSafe}></button>
+                    <p className="vault">Метод "{safe}"</p>
+                    <p className="vault_description">{safeDescription}</p>
 
 
-                    <button className="latte-img"></button>
-                    <p className="latte">Метод “Эффект Латте”</p>
-                    <p className="latte_description">description</p>
+                    <button className="null-img" onClick={onClickZeroing}></button>
+                    <p className="null">Метод "{zeroing}"</p>
+                    <p className="null_description">{zeroingDescription}</p>
 
-                    <button className="parts-img"></button>
-                    <p className="parts">Метод “50/30/20”</p>
-                    <p className="parts_description">description</p>
+
+                    <button className="latte-img" onClick={onClickLatte}></button>
+                    <p className="latte">Метод "{latte}"</p>
+                    <p className="latte_description">{latteDescription}</p>
+
+                    <button className="parts-img" onClick={onClickPopular}></button>
+                    <p className="parts">Метод "{popular}"</p>
+                    <p className="parts_description">{popularDescription}</p>
                 </div>
             </div>
 
