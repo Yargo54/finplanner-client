09db0d19afda37a78f849d63119ef652b0ffbd1f
AleksandrHexlet <46378431+AleksandrHexlet@users.noreply.github.com>
2021-06-06 19:41:24 +0300

    Add disign

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 export default function Header ( { photo, heading, info } ) {
     return (
         <div className="div-header">
-            <img src={photo} alt="photo" className="img-in-header"/>
+            {/* <img src={photo} alt="photo" className="img-in-header"/> */}
             <div className="div-content">
                 <h1 className="h1-header">{heading}</h1>
                 <p className="p-info-header">{info}</p>
