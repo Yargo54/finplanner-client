79bcd73ded28ff332fd1cb670026bfb6d3ce211d
Пользователь <vin123@mail.ru>
2021-06-07 12:39:54 +0300

    update registrationPage

--- src/pages/ChoiseProgram.js
+++ src/pages/ChoiseProgram.js
@@ -4,61 +4,63 @@ import './ChoiseProgram.css';
 
 
 function ChoiseProgram() {
-    
+
     let dataAccumulation;
 
-    let [ convert, setConvert ] = useState('');
-    let [ convertDescription, setConvertDescription ] = useState('');
+    let [convert, setConvert] = useState('');
+    let [convertDescription, setConvertDescription] = useState('');
 
-    let [ jug, setJug] = useState('');
-    let [ jugDescription, setJugDescription ] = useState('');
+    let [jug, setJug] = useState('');
+    let [jugDescription, setJugDescription] = useState('');
 
-    let [ safe, setSafe] = useState('');
-    let [ safeDescription, setSafeDescription ] = useState('');
+    let [safe, setSafe] = useState('');
+    let [safeDescription, setSafeDescription] = useState('');
 
-    let [ zeroing, setZeroing] = useState('');
-    let [ zeroingDescription, setZeroingDescription ] = useState('');
+    let [zeroing, setZeroing] = useState('');
+    let [zeroingDescription, setZeroingDescription] = useState('');
 
-    let [ latte, setLatte] = useState('');
-    let [ latteDescription, setLatteDescription ] = useState('');
+    let [latte, setLatte] = useState('');
+    let [latteDescription, setLatteDescription] = useState('');
 
-    let [ popular, setPopular] = useState('');
-    let [ popularDescription, setPopularDescription ] = useState('');
+    let [popular, setPopular] = useState('');
+    let [popularDescription, setPopularDescription] = useState('');
 
     useEffect(() => {
         fetch("http://localhost:3000/accumulation")
-        .then(res => res.json())
-        .then((data) => {
-            dataAccumulation = [...data];
-            dataAccumulation.forEach(element => {
-                switch(element.name){
-                    case "Четыре конверта":
-                        setConvert(element.name);
-                        setConvertDescription(element.short);
-                        break;
-                    case "Шесть кувшинов":
-                        setJug(element.name);
-                        setJugDescription(element.short);
-                        break;
-                    case "Сейф":
-                        setSafe(element.name);
-                        setSafeDescription(element.short);
-                        break;
-                    case "Обнуление":
-                        setZeroing(element.name);
-                        setZeroingDescription(element.short);
-                        break;
-                    case "Эффект латте":
-                        setLatte(element.name);
-                        setLatteDescription(element.short);
-                        break;
-                    case "50/30/20":
-                        setPopular(element.name);
-                        setPopularDescription(element.short);
-                        break;
-                }
-            });
-        })
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data)
+
+                dataAccumulation = [...data];
+                dataAccumulation.forEach(element => {
+                    switch (element.name) {
+                        case "Четыре конверта":
+                            setConvert(element.name);
+                            setConvertDescription(element.short);
+                            break;
+                        case "Шесть кувшинов":
+                            setJug(element.name);
+                            setJugDescription(element.short);
+                            break;
+                        case "Сейф":
+                            setSafe(element.name);
+                            setSafeDescription(element.short);
+                            break;
+                        case "Обнуление":
+                            setZeroing(element.name);
+                            setZeroingDescription(element.short);
+                            break;
+                        case "Эффект латте":
+                            setLatte(element.name);
+                            setLatteDescription(element.short);
+                            break;
+                        case "50/30/20":
+                            setPopular(element.name);
+                            setPopularDescription(element.short);
+                            break;
+                    }
+                });
+            })
     }, [])
 
     let history = useHistory();
@@ -86,34 +88,35 @@ function ChoiseProgram() {
     function onClickPopular() {
         history.push('/accumulationprogrampopular');
     }
-    
+
     return (
         <div className="choise-program_wraper">
             <div className="main">
-                    <p className="schema">Выберете схему накопления</p>
+                <p className="schema">Выберете схему накопления</p>
 
 
-                    <div className="choise-program-bandle-one">
+                <div className="choise-program-bandle-one">
                         <div className="container">
-
-                            <button className="envelope-img"></button>
-                            <p className="four-envelope" >Метод “Четыре конверта”</p>
-                            <p className="four-envelope_description">description</p>
+                    <button className="envelope-img" onClick={onClickConvert}></button>
+                    {/* <p className="four-envelope" >Метод "{convert}"</p> */}
+                    <p className="four-envelope" >Метод "Четыре конверта"</p>
+                    <p className="four-envelope_description">{convertDescription}</p>
                         </div>
                   
                     <div className="container">
-
-                        <button className="jag-img"></button>
-                        <p className="six-jag">Метод “Шесть кувшинов”</p>
-                        <p className="six-jag_description">description</p>
+                    <button className="jag-img" onClick={onClickJug}></button>
+                    <p className="six-jag">Метод "Шесть кувшинов"</p>
+                    {/* <p className="six-jag">Метод "{jug}"</p> */}
+                    <p className="six-jag_description">{jugDescription}</p>
                     </div>
                 </div>
                 <div className="choise-program-bandle-one">
                     <div className="container">
+                    <button className="vault-img" onClick={onClickSafe}></button>
+                    <p className="vault">Метод "Сейф"</p>
+                    {/* <p className="vault">Метод "{safe}"</p> */}
+                    <p className="vault_description">{safeDescription}</p>
 
-                        <button className="vault-img"></button>
-                        <p className="vault">Метод “Сейф”</p>
-                        <p className="vault_description">description</p>
                     </div>
                     <div className="container">
 
@@ -124,49 +127,37 @@ function ChoiseProgram() {
                 </div>
                 <div className="choise-program-bandle-one">
                     <div className="container">
-
-                        <button className="latte-img"></button>
-                        <p className="latte">Метод “Эффект Латте”</p>
-                        <p className="latte_description">description</p>
+                        <button className="latte-img" onClick={onClickLatte}></button>
+                        <p className="latte">Метод "Эффект латте"</p>
+                        {/* <p className="latte">Метод "{latte}"</p> */}
+                        <p className="latte_description">{latteDescription}</p>
                     </div>
                     <div className="container">
 
-                        <button className="parts-img"></button>
-                        <p className="parts">Метод “50/30/20”</p>
-                        <p className="parts_description">description</p>
+                        <button className="parts-img" onClick={onClickPopular}></button>
+                        <p className="parts">Метод "50/30/20"</p>
+                        {/* <p className="parts">Метод "{popular}"</p> */}
+                        <p className="parts_description">{popularDescription}</p>
                     </div>
 
-                    <button className="envelope-img" onClick={onClickConvert}></button>
-                    <p className="four-envelope" >Метод "{convert}"</p>
-                    <p className="four-envelope_description">{convertDescription}</p>
-
-                    <button className="jag-img" onClick={onClickJug}></button>
-                    <p className="six-jag">Метод "{jug}"</p>
-                    <p className="six-jag_description">{jugDescription}</p>
-
-                    <button className="vault-img" onClick={onClickSafe}></button>
-                    <p className="vault">Метод "{safe}"</p>
-                    <p className="vault_description">{safeDescription}</p>
 
 
-                    <button className="null-img" onClick={onClickZeroing}></button>
-                    <p className="null">Метод "{zeroing}"</p>
-                    <p className="null_description">{zeroingDescription}</p>
 
 
-                    <button className="latte-img" onClick={onClickLatte}></button>
-                    <p className="latte">Метод "{latte}"</p>
-                    <p className="latte_description">{latteDescription}</p>
 
-                    <button className="parts-img" onClick={onClickPopular}></button>
-                    <p className="parts">Метод "{popular}"</p>
-                    <p className="parts_description">{popularDescription}</p>
+
+
+
+
+
+
+
 
                 </div>
             </div>
         </div>
 
-            
+
     )
 }
 export default ChoiseProgram
