cd1bb7db5cee2aadee3c6e3e27442a215f0b8518
Пользователь <vin123@mail.ru>
2021-06-08 16:14:43 +0300

    add Latte, Parts and Null scheme.Delete console.log

--- src/components/partsSchema.js
+++ src/components/partsSchema.js
@@ -9,28 +9,29 @@ export default function Partsschema() {
     const [basicValue, setbasicValue] = useState();
     const [desiredValue, setdesiredValue] = useState();
     const [accumulationValue, setaccumulationValue] = useState();
-console.log("accumulationValue",accumulationValue);
+
     let mandatorySpending =
-        <ul className="ul-mandatory_spending">
-            <li>платежи по кредитам</li>
-            <li>коммунальные услуги</li>
-            <li>оплата детского сада</li>
-            <li>машина, бензин</li>
-            <li>питание</li>
-        </ul>
-
-    let savings =
+            <ul className="ul-mandatory_spending">
+                <li>платежи по кредитам</li>
+                <li>коммунальные услуги</li>
+                <li>оплата детского сада</li>
+                <li>машина, бензин</li>
+                <li>питание</li>
+            </ul>
+    
+        let savings =
+            <ul className="ul-popularOptionalSpending">
+                <li>Сумма которую вы отложили на сбережения</li>
+            </ul>
+    
+        let popularOptionalSpending =
         <ul className="ul-popularOptionalSpending">
-            <li>Сумма которую вы отложили на сбережения</li>
+            <li>посещение кафе и ресторанов</li>
+            <li>покупка планшета, шубы</li>
+            <li>оплата активностей (тренажёрка, фитнес)</li>
+            <li>услуги парикмахера</li>
         </ul>
-
-    let popularOptionalSpending =
-    <ul className="ul-popularOptionalSpending">
-        <li>посещение кафе и ресторанов</li>
-        <li>покупка планшета, шубы</li>
-        <li>оплата активностей (тренажёрка, фитнес)</li>
-        <li>услуги парикмахера</li>
-    </ul>
+    
 
     function inputSave(event) {
         console.log("inputSave", event.target.value)
@@ -43,14 +44,9 @@ console.log("accumulationValue",accumulationValue);
         }, 400
         );
     }
-    // console.log("basicValue,inputSave41", basicValue)
-    // console.log("desiredValue,inputSave41", desiredValue)
-    // console.log("accumulationValue,inputSave 41", accumulationValue)
 
     function addAmount() {
-        // console.log('addAmount,basicValue', basicValue);
-        // console.log('addAmount,desiredValue', desiredValue);
-        // console.log('addAmount,accumulationValue 46', accumulationValue);
+
         fetch('http://localhost:3000/accumulationnew', {
             method: "POST",
             headers: {
@@ -65,27 +61,19 @@ console.log("accumulationValue",accumulationValue);
                 setbasicValue(newValue.basic);
                 setdesiredValue(newValue.desired)
                 setaccumulationValue(newValue.accumulation)
-                // console.log("basicValue", basicValue)
-                // console.log("desiredValue", desiredValue)
-                console.log("accumulationValue 63", accumulationValue)
-            });
-        // Тянем с сервера схему расчета и Добавляем в поле Input text="Сбережения" расчитанные значения
+
+             });        
     }
 
 
     return (
         <div>
             <AddAmount amount="добавить сэкономленную сумму..." inputSave={inputSave} addAmount={addAmount} />
-            {/* {   console.log("accumulationValue 72", accumulationValue),
-                console.log("basicValue72", basicValue),
-                console.log("desiredValue72", desiredValue)
-                } */}
+
             <Input text="На обязательные траты" classInput="accumulation" classInputPContent="accumulation-p" inputValue={basicValue} content={mandatorySpending} />
             <Input text="Расходы на жизнь" classInput="accumulation" classInputPContent="accumulation-p" inputValue={desiredValue} content={popularOptionalSpending} />
-            <Input text="Сбережения" classInput="accumulation" classInputPContent="accumulation-p" inputValue={accumulationValue} content={savings}/>
-        </div >
+            <Input text="Сбережения" classInput="accumulation" classInputPContent="accumulation-p" inputValue={accumulationValue} content={savings} />
+        </div>
     )
 
 }
-
-
